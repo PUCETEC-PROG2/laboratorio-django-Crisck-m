b3da85d264a1b6bf365723667261767bcb0b8d69
Cristian Monge <cemonge@puce.edu.ec>
2024-12-21 09:26:00 -0500

    Laboratorio N8 Final

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 def index(request):
-    pokemons = Pokemon.objects.all('name')
+    pokemons = Pokemon.objects.all()
+    trainers = Trainer.objects.all()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({'pokemons': pokemons}, request))
+    return HttpResponse(template.render({
+        'pokemons': pokemons,
+        'trainers': trainers
+        },
+        request))
 
 def pokemon(request, pokemon_id):
     pokemon = Pokemon.objects.get(pk = pokemon_id)
@@ -14,3 +19,13 @@ def pokemon(request, pokemon_id):
         'pokemon': pokemon
     }
     return HttpResponse(template.render(context, request))
+
+
+def trainer_detail(request, trainer_id):
+    trainer = Trainer.objects.get(pk = trainer_id)
+    template = loader.get_template('display_trainer.html')
+    context = {
+        'trainer': trainer
+    }
+    return HttpResponse(template.render(context, request))
+
